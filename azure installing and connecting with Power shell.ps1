813961857443c12a42a555539240c4f174015b41
HANEEF1997 <75823437+HANEEF1997@users.noreply.github.com>
2021-06-07 23:26:20 +0530

    Add files via upload

---
+++ azure installing and connecting with Power shell.ps1
@@ -0,0 +1,18 @@
+﻿# installing and connecting azure
+Install-Module -name azureAD
+get-module azuread
+Connect-AzureAD
+
+# getting the user who are in azure
+get-azureaduser
+
+# filtering the user by letters
+get-azureaduser -filter "startswith(givenName,'a')"
+
+# creating new user in azure
+#first create password
+$userspassword = new-object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$userspassword.Password = "delta98741!"
+
+
+New-AzureADUser -DisplayName "shaik" -PasswordProfile $userspassword -UserPrincipalName shaikhaneef42@gmail.com -MailNickName "shaikhaneef1997"
